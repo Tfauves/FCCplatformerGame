68f81e97261e07841d12d4816e91ee007d8326c7
Travis J <tfauvelle@gmail.com>
2024-02-24 17:23:49 -0500

    add canvas and player obj

--- script.js
+++ script.js
@@ -2,3 +2,28 @@ const startBtn = document.getElementById("start-btn");
 const canvas = document.getElementById("canvas");
 const startScreen = document.querySelector(".start-screen");
 const checkpointScreen = document.querySelector(".checkpoint-screen");
+const checkpointMessage = document.querySelector(".checkpoint-screen > p");
+const ctx = canvas.getContext("2d");
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+const gravity = 0.5;
+let isCheckpointCollisionDetectionActive = true;
+
+class Player {
+  constructor() {
+    this.position = {
+      x: 10,
+      y: 400,
+    };
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
+    this.width = 40;
+    this.height = 40;
+  }
+
+  draw() {
+    ctx.fillStyle = "#99c9ff";
+  }
+}
